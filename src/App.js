0664b43951489e5074a7c51477d0437c35cf9276
Jorge Mena <jormencar@gmail.com>
2022-03-19 14:03:00 +0000

    add nav-bar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import './App.css';
-import {Component} from 'react';
-import Productos from './components/Productos';
-import Title from './components/Title';
-import Navbar from './components/Navbar';
-import Layout from "./components/Layout.js"
+import "./App.css";
+import { Component } from "react";
+import Productos from "./components/Productos";
+import Title from "./components/Title";
+import Navbar from "./components/Navbar";
+import Layout from "./components/Layout.js";
 
 class App extends Component {
-    state = {
-        productos: [
-            {nombre: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
-            {nombre: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg'},
-            {nombre: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},
-        ],
-    };
+  state = {
+    productos: [
+      { nombre: "Tomate", price: 1500, img: "/productos/tomate.jpg" },
+      { nombre: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
+      { nombre: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
+    ],
+  };
 
-    render() {
-        return (
-            <div>
-                <Navbar/>
-                <Layout>
-                    <Title/>
-                    <Productos
-                        agregarAlCarro={() => console.log('No hace nada')}
-                        items={this.state.productos}
-                    />
-                </Layout>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={() => console.log("No hace nada")}
+            items={this.state.productos}
+          />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
